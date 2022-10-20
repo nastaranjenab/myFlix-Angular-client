87cf3cc7ef0c9b30eca83d406e72d0667474c76e
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-10-20 13:44:08 +0200

    adding & setting movie-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 
 
@@ -25,7 +26,14 @@ export class AppComponent {
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       // Assign dialog width
-      width: '280px'
+      width: '480px'
+    });
+  }
+
+  //opens the all movies window
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 }
